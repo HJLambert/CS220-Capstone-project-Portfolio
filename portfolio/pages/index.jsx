93ad2807d72de53bf192b2c4cc6e9748adc15dce
HJLambert <hunter.j.lambert@gmail.com>
2025-10-05 06:49:04 +0000

    Edited home contents

--- portfolio/pages/index.jsx
+++ portfolio/pages/index.jsx
@@ -18,9 +18,9 @@ export default function Home() {
         <Navbar options={navOptions} />
 
         <main className="max-w-4xl mx-auto px-8 py-20">
-          <h1 className="text-4xl font-bold mb-4">Hi, I’m Hunter Lambert 👋</h1>
+          <h1 className="text-4xl font-bold mb-4">Hi, I’m Hunter Lambert </h1>
           <p className="text-lg text-gray-600 dark:text-gray-300 mb-6">
-            I’m a web developer and biology student interested in creative, scientific, and historical applications of software. 
+            I’m a web developer student interested in creative, scientific, and historical applications of software. 
             Welcome to my personal portfolio.
           </p>
           <ThemeToggle />
